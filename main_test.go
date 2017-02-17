2249c695a05294de0b7f4f83375f2ff1f67cfb36
Marius Orcsik <marius@habarnam.ro>
2017-02-17 18:32:25 +0100

    go fmt improvements

--- main_test.go
+++ main_test.go
@@ -1,261 +1,261 @@
 package main
 
 import (
-    "./parser"
-    "testing"
-    "os"
-    "io"
-    "bytes"
-    "strings"
+	"./parser"
+	"bytes"
+	"io"
+	"os"
+	"strings"
+	"testing"
 )
 
 type testPair struct {
-    text        string
-    expected    bool
-    doc         parser.Document
+	text     string
+	expected bool
+	doc      parser.Document
 }
 
 var emptyDoc parser.Document = parser.Document{}
 
 var someTests = []testPair{
-//    {
-//        "\u0000",
-//        true,
-//        parser.Document{
-//            Children: []parser.Node{
-//                parser.Node{
-//                    Type: parser.Par,
-//                    Content: []byte("\ufffd"),
-//                },
-//            },
-//        },
-//    },
-    {
-        "[ana](httpslittrme)",
-        true,
-        emptyDoc,
-    },
-    {
-        "[ana](https://littr.me)",
-        true,
-        emptyDoc,
-    },
-    {
-        "",
-        false,
-        emptyDoc,
-    },
-    {
-        "some text before [test 123](https://littr.me)", 
-        true,
-        emptyDoc,
-    },
-    {
-        "[test 123](https://littr.me) some text after", 
-        true,
-        emptyDoc,
-    },
-    {
-        "some text before [test 123](https://littr.me) some text after", 
-        true,
-        emptyDoc,
-    },
-    {
-        "𐍈ᏚᎢᎵᎬᎢᎬᏒăîțș\n", 
-        true ,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.Par,
-                    Content: []byte("𐍈ᏚᎢᎵᎬᎢᎬᏒăîțș"),
-                },
-            },
-        },
-    },
-    {
-        " ---\n",
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.TBreak,
-                    Content: []byte("-"),
-                },
-            },
-        },
-    },
-    {
-        "  ***\n",
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.TBreak,
-                    Content: []byte("-"),
-                },
-            },
-        },
-    },
-    {
-        "  * * * *\n",
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.TBreak,
-                    Content: []byte("-"),
-                },
-            },
-        },
-    },
-    {
-        "   ___\r",
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.TBreak,
-                    Content: []byte("-"),
-                },
-            },
-        },
-    },
-    {
-        "   _*-*__\r",
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.Par,
-                    Content: []byte("   _*-*__"),
-                },
-            },
-        },
-    },
-    {
-        "# ana are mere\n", 
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.H1,
-                    Content: []byte("ana are mere"),
-                },
-            },
-        },
-    },
-    {
-        "## ana are mere\n", 
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.H2,
-                    Content: []byte("ana are mere"),
-                },
-            },
-        },
-    },
+	//    {
+	//        "\u0000",
+	//        true,
+	//        parser.Document{
+	//            Children: []parser.Node{
+	//                parser.Node{
+	//                    Type: parser.Par,
+	//                    Content: []byte("\ufffd"),
+	//                },
+	//            },
+	//        },
+	//    },
+	{
+		"[ana](httpslittrme)",
+		true,
+		emptyDoc,
+	},
+	{
+		"[ana](https://littr.me)",
+		true,
+		emptyDoc,
+	},
+	{
+		"",
+		false,
+		emptyDoc,
+	},
+	{
+		"some text before [test 123](https://littr.me)",
+		true,
+		emptyDoc,
+	},
+	{
+		"[test 123](https://littr.me) some text after",
+		true,
+		emptyDoc,
+	},
+	{
+		"some text before [test 123](https://littr.me) some text after",
+		true,
+		emptyDoc,
+	},
+	{
+		"𐍈ᏚᎢᎵᎬᎢᎬᏒăîțș\n",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.Par,
+					Content: []byte("𐍈ᏚᎢᎵᎬᎢᎬᏒăîțș"),
+				},
+			},
+		},
+	},
+	{
+		" ---\n",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.TBreak,
+					Content: []byte("-"),
+				},
+			},
+		},
+	},
+	{
+		"  ***\n",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.TBreak,
+					Content: []byte("-"),
+				},
+			},
+		},
+	},
+	{
+		"  * * * *\n",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.TBreak,
+					Content: []byte("-"),
+				},
+			},
+		},
+	},
+	{
+		"   ___\r",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.TBreak,
+					Content: []byte("-"),
+				},
+			},
+		},
+	},
+	{
+		"   _*-*__\r",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.Par,
+					Content: []byte("   _*-*__"),
+				},
+			},
+		},
+	},
+	{
+		"# ana are mere\n",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.H1,
+					Content: []byte("ana are mere"),
+				},
+			},
+		},
+	},
+	{
+		"## ana are mere\n",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.H2,
+					Content: []byte("ana are mere"),
+				},
+			},
+		},
+	},
 
-    {
-        "### ana are mere\n",
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.H3,
-                    Content: []byte("ana are mere"),
-                },
-            },
-        },
-    },
-    {
-        "#### ana are mere\n",
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.H4,
-                    Content: []byte("ana are mere"),
-                },
-            },
-        },
-    },
-    {
-        "##### ana-are-mere\n",
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.H5,
-                    Content: []byte("ana-are-mere"),
-                },
-            },
-        },
-    },
-    {
-        "###### ana-are-mere\n",
-        true,
-        parser.Document{
-            Children: []parser.Node{
-                parser.Node{
-                    Type: parser.H6,
-                    Content: []byte("ana-are-mere"),
-                },
-            },
-        },
-    },
+	{
+		"### ana are mere\n",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.H3,
+					Content: []byte("ana are mere"),
+				},
+			},
+		},
+	},
+	{
+		"#### ana are mere\n",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.H4,
+					Content: []byte("ana are mere"),
+				},
+			},
+		},
+	},
+	{
+		"##### ana-are-mere\n",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.H5,
+					Content: []byte("ana-are-mere"),
+				},
+			},
+		},
+	},
+	{
+		"###### ana-are-mere\n",
+		true,
+		parser.Document{
+			Children: []parser.Node{
+				parser.Node{
+					Type:    parser.H6,
+					Content: []byte("ana-are-mere"),
+				},
+			},
+		},
+	},
 }
 
-func TestParse (t *testing.T) {
-//    var n parser.Node
-//    t.Logf("%s", n.String())
+func TestParse(t *testing.T) {
+	//    var n parser.Node
+	//    t.Logf("%s", n.String())
 
-    for _, curTest := range someTests {
-        doc, err  := parser.Parse([]byte(curTest.text))
-        t.Logf("Testing %q", strings.Trim(curTest.text, "\n\r"))
+	for _, curTest := range someTests {
+		doc, err := parser.Parse([]byte(curTest.text))
+		t.Logf("Testing %q", strings.Trim(curTest.text, "\n\r"))
 
-        if err != nil && curTest.expected {
-            t.Errorf("Parse result invalid, expected '%t, got %v'\n", curTest.expected)
-        }
+		if err != nil && curTest.expected {
+			t.Errorf("Parse result invalid, expected '%t, got %v'\n", curTest.expected)
+		}
 
-        if !curTest.doc.Equal(doc) {
-            t.Errorf("Expected %q\nGot %q", curTest.doc.String(), doc.String())
-        }
+		if !curTest.doc.Equal(doc) {
+			t.Errorf("Expected %q\nGot %q", curTest.doc.String(), doc.String())
+		}
 
-        testChildren := curTest.doc.Children
-        children := doc.Children
-        if len(testChildren) != len(children) {
-            t.Errorf("Parse result invalid, children length expected %d != %d", len(testChildren), len(children))
-        }
+		testChildren := curTest.doc.Children
+		children := doc.Children
+		if len(testChildren) != len(children) {
+			t.Errorf("Parse result invalid, children length expected %d != %d", len(testChildren), len(children))
+		}
 
-        if len(testChildren) > 0 && len(children) > 0{
-            testNode := testChildren[0]
-            node := children[0]
-            if testNode.Type != node.Type {
-                t.Errorf("Parse result invalid, node type expected %v != %v", testNode.Type, node.Type)
-            }
-            if !bytes.Equal(testNode.Content, node.Content) {
-                t.Errorf("Parse result invalid, node content expected %s != %s", testNode.Content, node.Content)
-                t.Errorf("Parse result invalid, node content expected %v != %v", testNode.Content, node.Content)
-            }
-            t.Logf("%s", doc.String())
-        }
-    }
+		if len(testChildren) > 0 && len(children) > 0 {
+			testNode := testChildren[0]
+			node := children[0]
+			if testNode.Type != node.Type {
+				t.Errorf("Parse result invalid, node type expected %v != %v", testNode.Type, node.Type)
+			}
+			if !bytes.Equal(testNode.Content, node.Content) {
+				t.Errorf("Parse result invalid, node content expected %s != %s", testNode.Content, node.Content)
+				t.Errorf("Parse result invalid, node content expected %v != %v", testNode.Content, node.Content)
+			}
+			t.Logf("%s", doc.String())
+		}
+	}
 }
 
-func TestParseReadme (t *testing.T) {
-    f, _ := os.Open("README.md")
+func TestParseReadme(t *testing.T) {
+	f, _ := os.Open("README.md")
 
-    data := make([]byte, 512)
-    io.ReadFull(f, data)
-    data = bytes.Trim(data, "\x00")
+	data := make([]byte, 512)
+	io.ReadFull(f, data)
+	data = bytes.Trim(data, "\x00")
 
-    if _, err := parser.Parse(data); err != nil {
-        t.Errorf("\tParse invalid\n")
-    }
+	if _, err := parser.Parse(data); err != nil {
+		t.Errorf("\tParse invalid\n")
+	}
 }
 
 func TestMain(m *testing.M) {
-    os.Exit(m.Run())
+	os.Exit(m.Run())
 }
--- parser/elements.go
+++ parser/elements.go
@@ -1,152 +1,152 @@
 package parser
 
 import (
-    "fmt"
-    "bytes"
+	"bytes"
+	"fmt"
 )
 
 func NewParagraph(cont []byte) Node {
-    var el Node
+	var el Node
 
-    el.Type = Par
-    el.Content = cont
+	el.Type = Par
+	el.Content = cont
 
-    return el
+	return el
 }
 
 func NewThematicBreak(t byte) Node {
-    var el Node
+	var el Node
 
-    el.Type = TBreak
-    el.Content = append(el.Content, t)
+	el.Type = TBreak
+	el.Content = append(el.Content, t)
 
-    return el
+	return el
 }
 
 func NewHeader(level uint, content []byte) Node {
-    var el Node
-    var t NodeType
+	var el Node
+	var t NodeType
 
-    switch level {
-    case 1:
-        t = H1
-    case 2:
-        t = H2
-    case 3:
-        t = H3
-    case 4:
-        t = H4
-    case 5:
-        t = H5
-    case 6:
-        t= H6
-    }
-    el.Type = t
-    el.Content = content
+	switch level {
+	case 1:
+		t = H1
+	case 2:
+		t = H2
+	case 3:
+		t = H3
+	case 4:
+		t = H4
+	case 5:
+		t = H5
+	case 6:
+		t = H6
+	}
+	el.Type = t
+	el.Content = content
 
-    return el
+	return el
 }
 
 func (n *Node) Equal(tn Node) bool {
-    if n.Type != tn.Type {
-        return false
-    }
-    if !bytes.Equal(n.Content, tn.Content) {
-        return false
-    }
-    if len(n.Children) != len(tn.Children) {
-        return false
-    }
-    for i, c := range(n.Children) {
-        if !c.Equal(tn.Children[i]) {
-            return false
-        }
-    }
-    return true
+	if n.Type != tn.Type {
+		return false
+	}
+	if !bytes.Equal(n.Content, tn.Content) {
+		return false
+	}
+	if len(n.Children) != len(tn.Children) {
+		return false
+	}
+	for i, c := range n.Children {
+		if !c.Equal(tn.Children[i]) {
+			return false
+		}
+	}
+	return true
 }
 
 func (d *Document) Equal(td Document) bool {
-    if len(d.Children) != len(td.Children) {
-        return false
-    }
-    for i, c := range(d.Children) {
-        if !c.Equal(td.Children[i]) {
-            return false
-        }
-    }
-    return true
+	if len(d.Children) != len(td.Children) {
+		return false
+	}
+	for i, c := range d.Children {
+		if !c.Equal(td.Children[i]) {
+			return false
+		}
+	}
+	return true
 }
 
 func (d *Document) String() string {
-    var buffer bytes.Buffer
-    for _, c := range(d.Children) {
-        buffer.WriteString(c.String())
-    }
-    return buffer.String()
+	var buffer bytes.Buffer
+	for _, c := range d.Children {
+		buffer.WriteString(c.String())
+	}
+	return buffer.String()
 }
 
 func (n *Node) String() string {
-    var buffer bytes.Buffer
-    buffer.WriteString(fmt.Sprintf("[%s] %s", n.Type.String(), string(n.Content)))
-    if len(n.Children) > 0  {
-        buffer.WriteString("\n[")
-    }
-    for _, c := range(n.Children) {
-        buffer.WriteString(fmt.Sprintf("\t%s",c.String()))
-    }
-    if len(n.Children) > 0  {
-        buffer.WriteString("]")
-    }
-    return buffer.String()
+	var buffer bytes.Buffer
+	buffer.WriteString(fmt.Sprintf("[%s] %s", n.Type.String(), string(n.Content)))
+	if len(n.Children) > 0 {
+		buffer.WriteString("\n[")
+	}
+	for _, c := range n.Children {
+		buffer.WriteString(fmt.Sprintf("\t%s", c.String()))
+	}
+	if len(n.Children) > 0 {
+		buffer.WriteString("]")
+	}
+	return buffer.String()
 }
 
 type NodeType uint8
 
 const (
-    None NodeType = iota 
-    Doc
-    H1
-    H2
-    H3
-    H4
-    H5
-    H6
-    Par
-    TBreak
+	None NodeType = iota
+	Doc
+	H1
+	H2
+	H3
+	H4
+	H5
+	H6
+	Par
+	TBreak
 )
 
 func (nt *NodeType) String() string {
-    switch *nt {
-    case Doc:
-        return "doc"
-    case Par:
-        return "par"
-    case TBreak:
-        return "tbr"
-    case H1:
-       return "h1"
-    case H2:
-       return "h2"
-    case H3:
-       return "h3"
-    case H4:
-       return "h4"
-    case H5:
-       return "h5"
-    case H6:
-       return "h6"
-   default:
-       return "nil"
-    }
+	switch *nt {
+	case Doc:
+		return "doc"
+	case Par:
+		return "par"
+	case TBreak:
+		return "tbr"
+	case H1:
+		return "h1"
+	case H2:
+		return "h2"
+	case H3:
+		return "h3"
+	case H4:
+		return "h4"
+	case H5:
+		return "h5"
+	case H6:
+		return "h6"
+	default:
+		return "nil"
+	}
 }
 
 type Document struct {
-    Children []Node
+	Children []Node
 }
 
 type Node struct {
-   Type NodeType
-   Content []byte
-   Children []Node
-   //Attributes map[string]string
+	Type     NodeType
+	Content  []byte
+	Children []Node
+	//Attributes map[string]string
 }
